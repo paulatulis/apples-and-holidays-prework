0c647afe7d9329c7cc6dfadc421873759063d865
Paula Tulis <tulis.paula@gmail.com>
2019-03-20 12:02:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,8 +54,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter][:christmas][:new_years]
-return holiday_hash.split(", ")
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
