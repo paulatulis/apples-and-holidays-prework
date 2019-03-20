9702291829b5cd9e31ce4853d0d0a097f27a9247
Paula Tulis <tulis.paula@gmail.com>
2019-03-20 11:20:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,9 +19,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # }
   # return the second element in the 4th of July array
   holiday_hash.collect do |holiday, thing|
-    thing 
+    thing[1]
 end
-return thing[1]
 end 
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
