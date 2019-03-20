0d499ec05e6e6482a23b92787aab50b06d524da6
Paula Tulis <tulis.paula@gmail.com>
2019-03-20 11:41:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,7 +34,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 holiday_hash.each do |season, data|
     if season == :winter 
-      data << supply 
+      data[]
 end
 end
 end 
