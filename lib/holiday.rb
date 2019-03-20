5d6c55cf674259647f55f3b53ab4b8dabb5d435f
Paula Tulis <tulis.paula@gmail.com>
2019-03-20 12:01:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,6 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter][:christmas][:new_years]
 return holiday_hash.split(", ")
 end
-end 
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
